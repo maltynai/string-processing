4b4ab8e13d58d7d3255f3fb42bed91cc0b5b7999
maltynai <120545181+maltynai@users.noreply.github.com>
2023-12-23 12:25:11 +0600

    Update StringProcessor.java

--- src/main/java/org/example/StringProcessor.java
+++ src/main/java/org/example/StringProcessor.java
@@ -19,6 +19,55 @@ public class StringProcessor {
         String[] words = sentence.split("\\s+");
         return words.length;
     }
+    public double calculateExpression(String expression){
+        expression = expression.replaceAll("\\s", "");
+        char[] arr = expression.toCharArray(); // make is aray
+        int n = 0;
+        char lastOperator = '+';
+        int result = 0;
+        for (char currentChar : arr) {
+            if (Character.isDigit(currentChar)) {
+                n = n * 10 + (currentChar - '0'); //if number is greater then zero
+            } else {
+                switch (lastOperator) {
+                    case '*':
+                        result *= n;
+                        break;
+                    case '/':
+                        result /= n;
+                        break;
+                    case '+':
+                        result += n;
+                        break;
+                    case '-':
+                        result -= n;
+                        break;
+                    default:
+                        break;
+                }
+                lastOperator = currentChar;
+
+                n = 0;
+            }
+        }
+        switch (lastOperator) {
+            case '+':
+                result += n;
+                break;
+            case '-':
+                result -= n;
+                break;
+            case '*':
+                result *= n;
+                break;
+            case '/':
+                result /= n;
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
 
 
 
